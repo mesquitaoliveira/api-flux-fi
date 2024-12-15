84b33bf706b054ff77d3e95ac037f8019263dcfc
Mesquita Oliveira <ismael.mesquita.100@gmail.com>
2024-12-15 18:36:49 -0300

    feat: update payment webhook to use 'asaas-access-token' for authentication and improve event logging

--- src/routes/payment-webhook.ts
+++ src/routes/payment-webhook.ts
@@ -1,18 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { Router, Request, Response } from "express";
+
 const paymentWebhookRouter = Router();
 
 paymentWebhookRouter.post(
   "/payment-webhook",
   async (req: Request, res: Response): Promise<void> => {
     try {
-      const authToken = req.headers["authToken"] as string | undefined;
-      const expectedAuthToken = process.env.WEBHOOK_AUTH_TOKEN;
-      console.log("authToken:", expectedAuthToken);
+      // Obter o token do cabeçalho 'asaas-access-token'
+      const accessToken = req.headers["asaas-access-token"] as
+        | string
+        | undefined;
+      const expectedAccessToken = process.env.WEBHOOK_AUTH_TOKEN;
+
+      console.log("Token esperado:", expectedAccessToken);
+      console.log("Token recebido:", accessToken);
+
       // Validação do token
-      if (!authToken || authToken !== expectedAuthToken) {
-        console.error("Token inválido:", authToken);
+      if (!accessToken || accessToken !== expectedAccessToken) {
+        console.error("Token inválido:", accessToken);
         res.status(403).json({ error: "Token inválido" });
         return;
       }
@@ -20,22 +27,32 @@ paymentWebhookRouter.post(
       // Processamento do payload do webhook
       const { event, payment } = req.body;
 
+      // Log do evento recebido
+      console.log("Evento recebido:", event);
+      console.log("Dados do pagamento:", payment);
+
       // Lógica do evento
-      if (event === "PAYMENT_RECEIVED") {
-        console.log(
-          `Pagamento recebido: ${payment.id}, valor: ${payment.value}`
-        );
-      } else if (event === "PAYMENT_CONFIRMED") {
-        console.log(
-          `Pagamento confirmado: ${payment.id}, valor: ${payment.value}`
-        );
-      } else {
-        console.warn(`Evento desconhecido: ${event}`);
+      switch (event) {
+        case "PAYMENT_RECEIVED":
+          console.log(
+            `Pagamento recebido: ${payment.id}, valor: ${payment.value}`
+          );
+          break;
+
+        case "PAYMENT_CONFIRMED":
+          console.log(
+            `Pagamento confirmado: ${payment.id}, valor: ${payment.value}`
+          );
+          break;
+
+        default:
+          console.warn(`Evento desconhecido: ${event}`);
       }
 
+      // Resposta de sucesso para o Asaas
       res.status(200).json({ message: "Webhook processado com sucesso" });
     } catch (error) {
-      console.error("Erro ao processar webhook:", (error as any).message);
+      console.error("Erro ao processar webhook:", error);
       res.status(500).json({ error: "Erro ao processar webhook" });
     }
   }
